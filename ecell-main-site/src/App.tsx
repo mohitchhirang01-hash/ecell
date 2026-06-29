bc10e6e1b9af0ea52d623e502ffb9472138da7c4
Mohit <mohitchhirang01@gmail.com>
2026-06-29 12:01:42 +0530

    dummy-hero-secction-created

--- ecell-main-site/src/App.tsx
+++ ecell-main-site/src/App.tsx
@@ -1,9 +1,25 @@
+import { useState } from 'react'
 import './App.css'
 import { Loader } from './components/Loader/Loader'
+import { Hero } from './components/hero/Hero'
 
 function App() {
+  const [showContent, setShowContent] = useState(false)
+  const [loaderDone, setLoaderDone] = useState(false)
+
   return (
-    <Loader subtitle="ECELL IITK" />
+    <>
+      {!loaderDone && (
+        <Loader
+          subtitle="ECELL IITK"
+          onTransitionStart={() => setShowContent(true)}
+          onComplete={() => setLoaderDone(true)}
+        />
+      )}
+      <div className={`app-content ${showContent ? 'visible' : ''}`}>
+        <Hero />
+      </div>
+    </>
   )
 }
 
